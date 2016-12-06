121258852b8a6cc3af38b3ccf96809d38d2f70ab
jose.egas <je052@hdm-stuttgart.de>
2016-12-06 13:53:19 +0100

    commit 6

--- Assets/Scripts/VRPawn.cs
+++ Assets/Scripts/VRPawn.cs
@@ -2,21 +2,31 @@
 using UnityEngine.Networking;
 using System.Collections;
 using System.Linq;
+using Leap.Unity;
 
 public class VRPawn : NetworkBehaviour {
 
     public Transform Head;
     public Transform LeftController;
     public Transform RightController;
+    public RiggedHand left, right;
 
 
     void Start () {
         if (isLocalPlayer) { 
             //GetComponentInChildren<SteamVR_ControllerManager>().enabled = true;
             GetComponentsInChildren<SteamVR_TrackedObject>(true).ToList().ForEach(x => x.enabled = true);
+            
             Head.GetComponentsInChildren<MeshRenderer>(true).ToList().ForEach(x => x.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly);
+
+        }else
+        {
+            GetComponentsInChildren<HandTransitionBehavior>(true).ToList().ForEach(x => x.enabled = false);
+            GetComponentsInChildren<IHandModel>(true).ToList().ForEach(x => x.enabled = false);
+            GetComponentsInChildren<RiggedFinger>(true).ToList().ForEach(x => x.enabled = false);
+            GetComponentsInChildren<RiggedHand>(true).ToList().ForEach(x => x.enabled = false);
         }
-	}
+    }
 
     void OnDestroy()
     {
